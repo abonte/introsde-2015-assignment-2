753fd8d582371853f6d061bd9c29efb80f924eb6
abonte <bonte@outlook.it>
2015-11-15 18:16:06 +0100

    Client steps from 3.6 to 3.8

--- src/ehealth/client/TestClient.java
+++ src/ehealth/client/TestClient.java
@@ -5,6 +5,7 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.lang.annotation.Target;
 import java.net.URI;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -26,13 +27,15 @@ import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientResponse;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 
 public class TestClient {
-	public static final String uriServer = "https://arcane-beach-6023.herokuapp.com/sdelab/"; //Carlo	
-	//public static final String uriServer = "https://peaceful-hamlet-5616.herokuapp.com/sdelab"; //Andrea
+	//public static final String uriServer = "https://arcane-beach-6023.herokuapp.com/sdelab/"; //Carlo	
+	public static final String uriServer = "https://peaceful-hamlet-5616.herokuapp.com/sdelab"; //Andrea
 	
 	private Client client = null;
 	private WebTarget service = null;
@@ -40,6 +43,11 @@ public class TestClient {
 	
 	private String first_person_id = null;
 	private String last_person_id = null;
+	private ArrayList<String> measure_types=new ArrayList<>();
+	private String measure_id = null;
+	private String measureType = null;
+
+	private String measure_id_person;
 	
 	public TestClient(){
 		clientConfig = new ClientConfig();
@@ -94,7 +102,7 @@ public class TestClient {
 			result = "ERROR";
 		}
 		
-		responseTemplate("2", "GET", response, "person/"+first_person_id, MediaType.APPLICATION_XML, result);
+		responseTemplate("2", "GET", response, "/person/"+first_person_id, MediaType.APPLICATION_XML, result);
 		System.out.print(prettyFormat(xml));
 	}
 	
@@ -120,7 +128,7 @@ public class TestClient {
 			result = "ERROR";
 		}
 		
-		responseTemplate("3", "PUT", response, "person/"+first_person_id, MediaType.APPLICATION_XML, result);
+		responseTemplate("3", "PUT", response, "/person/"+first_person_id, MediaType.APPLICATION_XML, result);
 		//System.out.print(prettyFormat(xml));
 	}
 	
@@ -161,7 +169,7 @@ public class TestClient {
 		} else {
 			result = "ERROR";
 		}
-		responseTemplate("4", "POST", response, "person/", MediaType.APPLICATION_XML, result);
+		responseTemplate("4", "POST", response, "/person/", MediaType.APPLICATION_XML, result);
 		System.out.print(prettyFormat(xml));
 		return rootElement.getFirstChild().getTextContent();
 	}
@@ -175,31 +183,113 @@ public class TestClient {
 	               .delete(Response.class);
 		String result = null;
 		Response responseGet = getPersonByid(person_id);
-		System.out.println(responseGet.getStatus());
+		
 		if (responseGet.getStatus() == 404) {
 			result = "OK";
 		}else{
 			result = "ERROR";
 		}
-		responseTemplate("5", "DELETE", response, "person/"+person_id, MediaType.APPLICATION_XML, result);
+		responseTemplate("5", "DELETE", response, "/person/"+person_id, MediaType.APPLICATION_XML, result);
+	}
+	
+	/**
+	 * Step 3.6. Follow now with the R#9 (GET BASE_URL/measureTypes).
+	 * If response contains more than 2 measureTypes
+	 * result is OK, else is ERROR (less than 3 measureTypes).
+	 * Save all measureTypes into array (measure_types
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws ParserConfigurationException )
+	 */
+	public void getMeasureTypes() throws ParserConfigurationException, SAXException, IOException {
+		Response response = service.path("measureTypes").request().accept(MediaType.APPLICATION_XML).get(Response.class);
+		String result = null;
+		String xml = response.readEntity(String.class);
+		Element rootElement = getRootElement(xml);
+		NodeList types = rootElement.getChildNodes(); 
+		for(int i = 0; i< types.getLength(); i++){
+			measure_types.add(types.item(i).getTextContent());
+		}
+		if(types.getLength() > 2){
+			result = "OK";
+		}else{
+			result = "ERROR";
+		}	
+		responseTemplate("5", "GET", response, "/measureTypes", MediaType.APPLICATION_XML, result);
+		System.out.print(prettyFormat(xml));
+	}
+	
+	/**
+	 * Step 3.7. Send R#6 (GET BASE_URL/person/{id}/{measureType}) for the first person you obtained at the beginning
+	 * and the last person, and for each measure types from measure_types.
+	 * If no response has at least one measure
+	 * result is ERROR (no data at all) else result is OK. Store one measure_id and one measureType.
+	 * @throws IOException 
+	 * @throws SAXException 
+	 * @throws ParserConfigurationException 
+	 */
+	public void getPersonHistoryByMeasureType() throws ParserConfigurationException, SAXException, IOException {
+		String result = "ERROR";		
+		ArrayList<String> xml_output = new ArrayList<String>();	
+		for(String id : new String[] {first_person_id, last_person_id}){
+			String xml_person = "";
+			for(String temp : measure_types){
+				//System.out.println("/person/"+id+"/"+temp);
+				String xml = service.path("/person/"+id+"/"+temp).request(MediaType.APPLICATION_XML)
+						.get(Response.class).readEntity(String.class);
+				Element rootElement = getRootElement(xml);
+				if(rootElement.getChildNodes().getLength() > 0){
+					xml_person = xml_person + prettyFormat(xml);
+					measure_id = rootElement.getFirstChild().getFirstChild().getTextContent();
+					measureType = temp;
+					measure_id_person = id;
+					result = "OK";
+				}
+			}
+			xml_output.add(xml_person);
+		}
 
+		responseTemplate("6", "GET", Response.ok().build(), "/person/{"+first_person_id+","+last_person_id+"}/{"
+				+measure_types.toString()+"}", MediaType.APPLICATION_XML, result);
+		System.out.println("First person, id = "+first_person_id);
+		System.out.println(xml_output.get(0));
+		System.out.println("Second person, id = "+last_person_id);
+		System.out.println(xml_output.get(1));
+	}
+	
+	/**
+	 * Step 3.8. Send R#7 (GET BASE_URL/person/{id}/{measureType}/{mid}) for the stored measure_id and measureType.
+	 * If the response is 200, result is OK, else is ERROR.
+	 */
+	public void	getMeasureHistoryById() {
+		String result = null;
+		Response response = service.path("person/"+measure_id_person+"/"+measureType+"/"+measure_id).request()
+				.accept(MediaType.APPLICATION_XML).get(Response.class);
+		if (response.getStatus() == 200) {
+			result = "OK";
+		}else{
+			result = "ERROR";
+		}
+		String xml = response.readEntity(String.class);
+		responseTemplate("7", "GET", response, "/person/"+measure_id_person+"/"+measureType+"/"+measure_id, MediaType.APPLICATION_XML, result);
+		System.out.println(xml);
 	}
 	
 	
 	public static void main(String[] args) {
 
-		/*ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-		WebTarget service = client.target(getBaseURI(uriServer));*/
+		
 		System.out.println("Server URL : " + uriServer);
 		try {
 			TestClient jerseyClient = new TestClient();
 			jerseyClient.getPeople(); //Step 3.1
 			jerseyClient.getPerson(); //Step 3.2		
 			jerseyClient.putPerson(); //Step 3.3
-			String person_id = jerseyClient.postPerson(); //Step 4.4
-			jerseyClient.deletePerson(person_id); //Step 4.5
-			
+			String person_id = jerseyClient.postPerson(); //Step 3.4
+			jerseyClient.deletePerson(person_id); //Step 3.5
+			jerseyClient.getMeasureTypes(); //Step 3.6
+			jerseyClient.getPersonHistoryByMeasureType(); // Step 3.7
+			jerseyClient.getMeasureHistoryById(); // Step 3.8
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
